f68bdbe5a7e431b05d2800b4d8172cb5e13392b6
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-14 12:01:13 -0500

    Update the hardhat config with key and etherscan items

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,10 +27,10 @@ module.exports = {
     },
     rinkeby: {
       url: secret.url,
-      accounts: "remote"  //This should be replace with [secret.key] when actually deploying to Rinkeby
+      accounts: [secret.key]  //This should be replace with [secret.key] when actually deploying to Rinkeby
     }
   },
   etherscan: {
-    apiKey: ""
+    apiKey: secret.etherscan
   },
 };
