2d942b5926d7d764f61406b29f448b2082e95334
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-16 06:26:49 -0500

    Added mint and burn functions

--- contracts/BaseErc20.sol
+++ contracts/BaseErc20.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BaseErc20 is ERC20 {
+contract BaseErc20 is ERC20, Ownable {
+    event TotalSharesIncreased(uint256);
+    event TotalSharesReduced(uint256);
+
     constructor(string memory _name, string memory _symbol, uint256 _supply) ERC20(_name, _symbol) {
         _mint(msg.sender, _supply);
+    }
+
+    function addShares(uint256 _quantity) public onlyOwner {
+        _mint(msg.sender, _quantity);
+        emit TotalSharesIncreased(_quantity);
+    }
+
+    function reduceShares(uint256 _quantity) public onlyOwner {
+        _burn(msg.sender, _quantity);
+        emit TotalSharesReduced(_quantity);
     }
 }
